7f15d42a9ebefa79a4d4b63a5c7a0a7ebc779b0c
remimoul <neptunes92390@outlook.fr>
2023-10-11 15:57:16 +0200

    connect mongoDB

--- app.js
+++ app.js
@@ -4,18 +4,8 @@ const port = 3000;
 const host = '0.0.0.0';
 
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://127.0.0.1:27017/apinode');
+mongoose.connect('mongodb://127.0.0.1:27017/musiquenode');
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-
-
-const postRoute = require('./routes/postRoute');
-//postRoute(app);
-app.use('/posts',postRoute);
-
-const commentRoute = require('./routes/commentRoute');
-app.use('/',commentRoute);
 
 
 app.listen(port, () => {
