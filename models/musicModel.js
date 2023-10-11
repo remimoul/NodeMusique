f6b42605295328ed4fab6e4932117b842c1b6e69
Rémi <115152594+remimoul@users.noreply.github.com>
2023-10-11 22:55:39 +0200

    add email in model

--- models/musicModel.js
+++ models/musicModel.js
@@ -13,6 +13,9 @@ let musicSchema = new Schema({
     prenom: {
         type:String,
     },
+    email: {
+      type:String,
+    },
     created_at: {
         type: Date,
         default: Date.now
