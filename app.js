5b9f4de1aee7d9d0a8e3a277937e0a397beaeede
remimoul <neptunes92390@outlook.fr>
2023-10-11 16:04:07 +0200

    added architecture MVC

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://127.0.0.1:27017/musiquenode');
 
 
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
